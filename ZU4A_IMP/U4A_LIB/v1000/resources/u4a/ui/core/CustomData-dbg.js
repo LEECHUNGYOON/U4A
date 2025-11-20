d57deec558c19f9e87b66dda30e3d466e8d06fd9
LEECHUNGYOON <soccerhs@gmail.com>
2025-11-20 11:08:58 +0900

    Update CustomData-dbg.js

--- ZU4A_IMP/U4A_LIB/v1000/resources/u4a/ui/core/CustomData-dbg.js
+++ ZU4A_IMP/U4A_LIB/v1000/resources/u4a/ui/core/CustomData-dbg.js
@@ -1,5 +1,17 @@
 // Copyright 2017. INFOCG Inc. all rights reserved.
-
+/**
+ * @since   2025-11-20 10:53:47
+ * @version v3.5.6-16
+ * @author  soccerhs
+ * @description
+ * 
+ * 기본 sap.ui.core.CustomData는 writeToDom 사용 시 DOM Attribute 이름에
+ * 자동으로 'data-*' prefix를 강제 부여한다.
+ *
+ * 본 클래스는 해당 prefix 강제를 제거하기 위해 확장된 CustomData 구현이다.
+ * writeToDom: true 로 설정하면 개발자가 지정한 key 그대로 DOM Attribute로 출력된다.
+ * 
+ */
 sap.ui.define("u4a.ui.core.CustomData", [
     "sap/ui/core/CustomData"
 ], function (CustomData) {
